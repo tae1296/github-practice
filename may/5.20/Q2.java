caa6dba5613daed0b519424d627cf5c316cf9d0a
tae1296 <84062289+tae1296@users.noreply.github.com>
2021-05-20 21:08:07 +0900

    Create Q2.java

---
+++ may/5.20/Q2.java
@@ -0,0 +1,42 @@
+package com.java.ArrayT;
+
+import java.util.Scanner;
+
+public class Q2 {
+//모르겠음
+	public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);  
+		
+	    System.out.println("숫자를 입력해주세요");
+	    int n=sc.nextInt();
+	    
+	    char A[][]=new char[n][n];
+	    int center=A.length/2;
+	    char ch='*';
+	    int begin=0;
+	    int end=A[0].length;
+	    
+	    for(int row=0;row<A.length;row++) {
+	    	if(row<=center) {
+	    		begin = row;
+	    		end = (A[row].length -1) -row;
+	    	}else{
+	    		begin = (A[row].length -1) - row; end = row;
+	    		
+	    	}for(int COL=begin;COL<=end;COL++){
+	    		 A[row][COL] = ch;
+	    		 }
+	    	}
+			  
+	    	for(int i = 0; i<A.length;i++){
+	    		for(int j =0; j< A[i].length;j++){
+			  if(A[i][j] == 0) {
+				  System.out.print(" ");
+				  }else{
+			  System.out.print(A[i][j]+""); 
+			  } 
+			  }
+	    	System.out.println(); }
+	    }
+	    
+}
