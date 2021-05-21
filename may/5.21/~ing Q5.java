ece54b69428310d1ebd66754dea499286126b6c9
tae1296 <84062289+tae1296@users.noreply.github.com>
2021-05-21 20:33:34 +0900

    Update ~ing Q5.java

--- may/5.21/~ing Q5.java
+++ may/5.21/~ing Q5.java
@@ -1,6 +1,4 @@
-package chapter5.Question;
-
-import java.util.Arrays;
+package com.java.chapter5;
 import java.util.Scanner;
 
 public class Q5 {
@@ -8,31 +6,26 @@ public class Q5 {
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
 		// 10개의 정수를 입력받아 배열에 넣고 저장한 후 내림차순으로 출력
-		
 		int[] Arr=new int[10];
 		int tmp=0;
+		
 		for(int i=0; i<Arr.length;i++) {
 			System.out.println((i+1)+"/"+Arr.length+" 숫자를 입력하세요");
 			Arr[i]=sc.nextInt();
-		}
-		
-		for(int i=0; i<Arr.length-1;i++) {
-			// [0] >= [1] true=> 0과 1번 자리 바꿈 false면 유지
-			
-			if(Arr[i]>=Arr[i+1]) {
-
-				tmp=Arr[i];
-				Arr[i]=Arr[i+1];
-				Arr[i+1]=tmp;
-			}else {
-				
 			}
-	
+		for(int i=0; i<Arr.length;i++) {
+			for(int j=0; j<Arr.length-1;j++) {
+				if(Arr[j]>Arr[j+1]) { // [0] >= [1] true=> 0과 1번 자리 바꿈 false면 유지
+					tmp=Arr[j];
+					Arr[j]=Arr[j+1];
+					Arr[j+1]=tmp;
+					}
+				}
+			}
+		
+		// 배열 출력
+		for(int i=Arr.length-1;i>0;i--) {			 
+				System.out.print(Arr[i]+" ");
 		}
-		
-		
-		System.out.println(Arrays.toString(Arr)); // 배열 출력
-		
 	}
-
 }
