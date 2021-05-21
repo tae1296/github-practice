2d431fca09783995504bc0b55379bec47e3c0d6b
tae1296 <84062289+tae1296@users.noreply.github.com>
2021-05-21 17:29:25 +0900

    Update StudentSumAvg.java

--- may/5_21/StudentSumAvg.java
+++ may/5_21/StudentSumAvg.java
@@ -5,24 +5,26 @@ import java.util.Scanner;
 public class StudentMainEx {
 
 	public static void main(String[] args) {
-		// student, score 클래스로 성적을 입력받아 student,score 클래스 객체로 배열을 다뤄보자.
 		Scanner sc=new Scanner(System.in);
+		
+		// student, score 클래스로 성적을 입력받아 student,score 클래스 객체로 배열을 다뤄보자.
+		
 		// student class : 이름, 나이, 학년, 주소, 점수: 과학/수학/영어
 		// score class : 과목/점수
+		
 		String[] str=new String[5];
 		Student stud[]=new Student[5];
+		
 		int math=0;
 		int sci=0;
 		int eng=0;
 		
-		for(int i=0; i<5;i++) {
+		for(int i=0; i<str.length;i++) {
 			System.out.println((i+1)+"/"+str.length+" 학생 성적을 입력하세요(이름/학년/주소/수학/과학/영어)");
 			str[i]=sc.next();
-			
 
 			String person = str[i];
 			String[] data=person.split("/");
-			
 			
 			stud[i] = new Student();
 			stud[i].name = data[0];
@@ -36,14 +38,12 @@ public class StudentMainEx {
 			stud[i].sco[1].score= Integer.parseInt(data[4]);
 			stud[i].sco[2].score= Integer.parseInt(data[5]);
 			
-//			System.out.println(stud[i].sco[0].score);
 			math+=stud[i].sco[0].score;
 			sci+=stud[i].sco[1].score;
 			eng+=stud[i].sco[2].score;
 			
 		}
-		
-		
+				
 		System.out.println("수학 : "+math+"/ "+(math/str.length));
 		System.out.println("과학 : "+sci+"/ "+(sci/str.length));
 		System.out.println("영어 : "+eng+"/ "+(eng/str.length));
