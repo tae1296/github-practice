78a73ee99dbd5d5a0524912cd4f1200bb5591e36
tae1296 <84062289+tae1296@users.noreply.github.com>
2021-05-24 14:31:56 +0900

    Create puppy.java

---
+++ may/5.24/puppy.java
@@ -0,0 +1,29 @@
+package com.java.chapter5;
+
+import java.util.Scanner;
+
+public class puppy {
+
+	public static void main(String[] args) {
+		Scanner sc =new Scanner(System.in);
+		
+		String[] nick= {"White", "Black", "Bear", "Sesame", "Gift"};
+		String[] name= {"흰둥이", "까만콩","곰돌이","깨소금","복돌이"};
+		
+		System.out.println("강아지 이름을 입력해주세요");
+		String str=sc.nextLine();
+		
+		for(int i=0;i<5;i++) {
+			if(str.equals(name[i])) {
+				System.out.println("> "+nick[i]);
+				break;
+			}else {
+				System.out.println("등록되지 않았습니다.");
+				break;
+			}
+		}
+		
+
+	}
+
+}
