bdf00ea1233ae6c01c9d233396ed46d87f21c28f
tae1296 <84062289+tae1296@users.noreply.github.com>
2021-05-26 17:40:46 +0900

    Create BirthDay.java

---
+++ eclipse-workspace/Java_basic/src/com/java/Cl/BirthDay.java
@@ -0,0 +1,17 @@
+package com.java.Cl;
+
+public class BirthDay {
+	int day;
+	int month;
+	int year;
+	public int getYear() {
+		return year;
+	}
+	public void setYear(int year) {
+		this.year = year;
+	}
+	
+	public void printThis() {
+		System.out.println(this);
+	}
+}
