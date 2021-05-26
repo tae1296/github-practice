1cfdad4a36e7b7b7ac377e3e58ba86f6e61525c5
tae1296 <84062289+tae1296@users.noreply.github.com>
2021-05-26 17:41:25 +0900

    Create Customer.java

---
+++ eclipse-workspace/Java_basic/src/com/java/Cl/P/Customer.java
@@ -0,0 +1,49 @@
+package com.java.Cl.P;
+
+public class Customer {
+	protected int customerID;
+	protected String customerName;
+	protected String customerGrade;
+	int bonusPoint;
+	double bonusRatio;
+	
+	public int getCustomerID() {
+		return customerID;
+	}
+
+	public void setCustomerID(int customerID) {
+		this.customerID = customerID;
+	}
+
+	public String getCustomerName() {
+		return customerName;
+	}
+
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+
+	public Customer() {
+		this(0," ");
+		customerGrade="SILVER";
+		bonusRatio= 0.01;
+		System.out.println("cutomer");
+}
+
+	public Customer(int id,String name) {
+		this.customerID=id;
+		this.customerName=name;
+		customerGrade="SILVER";
+		bonusRatio= 0.01;
+	}
+	
+	public int calcPrice(int price) {
+		bonusPoint +=price*bonusRatio;
+		return price;
+	}
+	
+	public String showCustomerInfo() {
+		return customerName + "님의 등급은 "+customerGrade+"이며, 보너스 포인트는 "+bonusPoint+"입니다.";
+	}
+	
+}
