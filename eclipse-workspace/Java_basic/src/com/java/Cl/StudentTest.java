5f19aac73964425010f748584e27a6337d7ddc80
tae1296 <84062289+tae1296@users.noreply.github.com>
2021-05-26 17:42:03 +0900

    Create StudentTest.java

---
+++ eclipse-workspace/Java_basic/src/com/java/Cl/StudentTest.java
@@ -0,0 +1,17 @@
+package com.java.Cl;
+
+public class StudentTest {
+
+	public static void main(String[] args) {
+		Student st=new Student();
+		
+		//st.studentName="홍길동";
+		
+		st.setStudentName("홍길동");
+		
+		
+		System.out.println(st.getStudentName());
+
+	}
+
+}
