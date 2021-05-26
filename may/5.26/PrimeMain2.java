619b571df59f6345dbf31cf8102f759fb8835332
tae1296 <84062289+tae1296@users.noreply.github.com>
2021-05-26 10:35:36 +0900

    Create PrimeMain2.java

---
+++ may/5.26/PrimeMain2.java
@@ -0,0 +1,68 @@
+public class PrimeMain2 {
+
+	public static void main(String[] args) {
+		Prime1 p1 = new Prime1();
+		p1.setNumber(50);
+		p1.primeNumber();
+		p1.print();
+
+		Prime1 p2 = new Prime1(100);
+		p2.primeNumber();
+		p2.print();
+	}
+
+}
+
+class Prime1 {
+	int number; // 대상 수
+	int[] primeNumbers;
+
+	Prime1() {
+		primeNumbers = new int[1000];
+		for (int i : primeNumbers) {
+			primeNumbers[i] = -1;
+		}
+	}
+
+	Prime1(int t) {
+		// this();
+		primeNumbers = new int[1000];
+		for (int i : primeNumbers) {
+			primeNumbers[i] = -1;
+		}
+		number = t;
+	}
+
+	void setNumber(int i) {
+		number = i;
+	}
+
+	boolean isPrime(int num) {
+		for (int i = 2; i < num; i++) {
+			if (num % i == 0)
+				return false;
+		}
+		return true;
+	}
+
+	void primeNumber() {
+		int index = 0;
+		for (int i = 2; i <= number; i++) {
+
+			if (isPrime(i)) { // 소수 일때만 값을 출력
+				// System.out.print(i + " ");
+				primeNumbers[index] = i;
+				index++;
+			}
+		}
+	}
+
+	void print() {
+		System.out.println(number + " 까지 소수는: ");
+		for (int i : primeNumbers) {
+			if(i!=0)System.out.print(i+" ");
+		}
+		System.out.println();
+	}
+
+}
