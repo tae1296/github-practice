fc5e234a17fc82eca4c895623160c3f5dca2a2ae
tae1296 <84062289+tae1296@users.noreply.github.com>
2021-05-26 17:42:06 +0900

    Create ThisExam.java

---
+++ eclipse-workspace/Java_basic/src/com/java/Cl/ThisExam.java
@@ -0,0 +1,14 @@
+package com.java.Cl;
+
+public class ThisExam {
+
+	public static void main(String[] args) {
+		BirthDay bDay = new BirthDay();
+		
+		bDay.setYear(2000);
+		
+		System.out.println(bDay);
+		bDay.printThis();
+	}
+
+}
