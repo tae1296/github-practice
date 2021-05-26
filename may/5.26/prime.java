85b63002bbfe4ea2d409d45faf9129f7a55c5700
tae1296 <84062289+tae1296@users.noreply.github.com>
2021-05-26 10:21:48 +0900

    Create prime.java

---
+++ may/5.26/prime.java
@@ -0,0 +1,40 @@
+package com.java.Cl;
+
+import java.util.Arrays;
+
+public class prime {
+	int num;
+	String[] prime;
+	
+	String str="";
+	
+	public prime() {
+		
+	}
+	
+
+	public void primeNum(int n) {
+		for(int i=2; i<=n;i++) {
+			boolean isPrime=true;
+			for(int j=2;j*j<=i;j++) {
+				if(i%j==0) {
+					isPrime=false;
+					break;
+				}
+			}
+			if(isPrime) {
+				str+=i+" ";
+			}
+		}
+		prime=str.split(" ");
+		
+	}
+	
+	public void setNum() {
+		
+	}
+	
+	public void print() {
+		System.out.println(Arrays.toString(prime));
+	}
+}
